135a819a683dd5a01b4a53620f5cf0b47c8bfc7b
Keturah Howard <keturah.dev@gmail.com>
2020-03-13 14:35:02 -0700

    create ability to fill out form of new Order. Info is not passed into Vendor yet

--- VendorOrder/Controllers/OrderController.cs
+++ VendorOrder/Controllers/OrderController.cs
@@ -12,5 +12,11 @@ namespace VendorOrder.Controllers
       Vendor currentVendor = Vendor.Find(Id);
       return View(currentVendor);
     }
+    [HttpPost("/vendors/{Id}/orders")]
+    public ActionResult Create(string title, string description, int price, string date, string ingredients)
+    {
+      Order newOrder = new Order(title, description, price, date, ingredients);
+      return RedirectToAction("Edit", newOrder);
+    }
   }
 }
