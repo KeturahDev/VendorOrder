48c182224bfc76e7517b3935755aaba5f29293c1
Keturah Howard <keturah.dev@gmail.com>
2020-03-13 19:38:42 -0700

    merge order and vendor into one controller, moved orders New into Vendor file, begin process of displaying order's to dynamic vendor paths

--- VendorOrder/Controllers/VendorController.cs
+++ VendorOrder/Controllers/VendorController.cs
@@ -41,6 +41,56 @@ namespace VendorOrder.Controllers
       Vendor thisVendor = Vendor.Find(Id);
       thisVendor.AddOrder(theOrder);
       return RedirectToAction("Show", Id);
+    
+    }
+
+      [HttpGet("/vendors/{Id}/orders/new")]
+    public ActionResult NewOrder( int Id) 
+    {
+      Vendor currentVendor = Vendor.Find(Id);
+      return View(currentVendor);
+    }
+    [HttpPost("/vendors/{Id}/orders")]
+    public ActionResult Create(string title, string description, int price, string date, string ingredients, int Id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      int thisId = Id;
+      Console.WriteLine("Id" + Id);
+      Order newOrder = new Order(title, description, price, date, ingredients);
+      model.Add("Id" ,Id);
+      model.Add("Order" ,newOrder);
+      return RedirectToAction("Update", model); 
     }
   }
 }
+
+// using Microsoft.AspNetCore.Mvc;
+// using System.Collections.Generic;
+// using VendorOrder.Models;
+// using System;
+
+// namespace VendorOrder.Controllers
+// {
+//   public class OrderController : Controller
+//   {
+//     [HttpGet("/vendors/{Id}/orders/new")]
+//     public ActionResult New( int Id) 
+//     {
+//       Vendor currentVendor = Vendor.Find(Id);
+//       return View(currentVendor);
+//     }
+//     [HttpPost("/vendors/{Id}/orders")]
+//     public ActionResult Create(string title, string description, int price, string date, string ingredients, int Id)
+//     {
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       int thisId = Id;
+//       Console.WriteLine("Id" + Id);
+//       Order newOrder = new Order(title, description, price, date, ingredients);
+//       model.Add("Id" ,Id);
+//       model.Add("Order" ,newOrder);
+//       return RedirectToAction("update", "vendors", model); //unable to reach Update in vendorscontroller~ unsure how
+//     }
+    
+    
+//   }
+// }
